e673872ecdd75df6ea849767b624a454809d2448
gys11@banbif.com.pe <gys11@banbif.com.pe>
2025-08-05 10:55:47 -0500

    Inicio

---
+++ app/index.tsx
@@ -0,0 +1,63 @@
+import { useRouter } from 'expo-router';
+import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+
+export default function Index() {
+
+  const router= useRouter();
+
+  return (
+   <View
+   style={{
+    flex: 1,
+    backgroundColor: 'white',
+   }}
+   >
+<Image source={require('./../assets/images/CasaPrado_Logo.png')}
+style={{
+  width: '100%',
+  height:300,
+  marginTop: 100
+}}></Image>
+  <View style={{
+    padding:35,
+    backgroundColor: '#b09977',
+    height: '100%',
+    borderTopLeftRadius: 35,
+    borderTopRightRadius: 35
+  }}>
+    <Text style={{
+      fontSize:35,
+      fontWeight: 'bold',
+      textAlign:'center',
+      color: 'white'
+    }}>Bienvenido a tu Tienda Virtual</Text>
+
+    <Text style={{
+      fontSize:20,
+      marginTop: 20,
+      textAlign:'center',
+      color: 'white'
+    }}>La elegancia del cuero en cada diseño, 100% cuero en el calzado por dentro y por fuera</Text>
+  
+    <TouchableOpacity style={styles.button} 
+      onPress={()=> router.push('/auth/signIn')} >
+        
+      <Text style={styles.buttonText} > Acceder </Text>
+    </TouchableOpacity>
+  </View>
+   </View>
+  );
+}
+const styles = StyleSheet.create({
+ button: {
+  padding:15,
+  backgroundColor:'white',
+  marginTop: 20,
+  borderRadius:10
+  },
+  buttonText:{
+    textAlign:'center',
+    fontSize:18
+  }
+ 
+});
